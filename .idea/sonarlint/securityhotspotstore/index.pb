ce98e8e80014ec0bfdf268bc430e4dabbf0c73bc
EvertonOliveiraFernandes <everton@umov.me>
2025-03-14 19:44:15 -0300

    Versao 1 do nosso sem visibilidade

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,7 @@
+
+=
+src/Main.java,9/2/924f089edb4af5ee45c8a9d1129cd28e2b368138
+?
+src/Pessoa.java,0/a/0ae21033df9e12143637b1a0506c6207b8c8c964
+>
+src/Carro.java,4/5/45eec078e283c1e800cca8ac5b757b6ee10fc2b8
---
+++ src/Carro.java
@@ -0,0 +1,13 @@
+public class Carro {
+    public String cor;
+    public String modelo;
+    public int anoDeFabricacao;
+    public String placa;
+
+    public Carro(String cor, String modelo, int anoDeFabricacao, String placa) {
+        this.cor = cor;
+        this.modelo = modelo;
+        this.anoDeFabricacao = anoDeFabricacao;
+        this.placa = placa;
+    }
+}

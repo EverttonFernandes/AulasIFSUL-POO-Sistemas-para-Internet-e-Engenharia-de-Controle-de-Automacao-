26292630fc67d844dd08b1f6a5b14322870b7b91
EvertonOliveiraFernandes <everton@umov.me>
2025-03-14 19:55:55 -0300

    Versão 2 do nosso carro

--- src/Carro.java
+++ src/Carro.java
@@ -3,11 +3,27 @@ public class Carro {
     public String modelo;
     public int anoDeFabricacao;
     public String placa;
+    private String codigoRenavan;
+    private String chassi;
 
-    public Carro(String cor, String modelo, int anoDeFabricacao, String placa) {
+    public Carro(String cor, String modelo, int anoDeFabricacao, String placa, String codigoRenavan, String chassi) {
         this.cor = cor;
         this.modelo = modelo;
         this.anoDeFabricacao = anoDeFabricacao;
         this.placa = placa;
+        this.codigoRenavan = codigoRenavan;
+        this.chassi = chassi;
     }
+
+    public String getCodigoRenavan(){
+        return this.codigoRenavan;
+    }
+
+    public String getChassi(){
+        return this.chassi;
+    }
+
+
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,16 @@ public class Main {
         Carro carro1 = new Carro("Vermelho",
                 "Jeep",
                 2023,
-                "KI9-8C66");
+                "KI9-8C66",
+                "23ASAG8838D281",
+                "281S78218DSDSAD218");
 
         System.out.println(carro1.cor);
         System.out.println(carro1.modelo);
         System.out.println(carro1.anoDeFabricacao);
         System.out.println(carro1.placa);
+        System.out.println(carro1.getCodigoRenavan());
+        System.out.println(carro1.getChassi());
 
     }
 }
